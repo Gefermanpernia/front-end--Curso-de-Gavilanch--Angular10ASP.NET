d36ac6aef79ff1bb73f10606702bb54302554848
Geferman Pernia <geferman.pro@gmail.com>
2020-11-14 00:51:02 -0400

    agregacion del ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearGenerosComponent } from './generos/crear-generos/crear-generos.component';
+import { IndiceGenersComponent } from './generos/indice-geners/indice-geners.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '', component: LandingPageComponent},
+ { path: 'generos', component: IndiceGenersComponent},
+ { path: 'generos', component: IndiceGenersComponent},
+ { path: 'generos/crear', component: CrearGenerosComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'front-end';
-
-  peliculasEnCines: { titulo: string; fechaLanzamiento: Date; precio: number; }[];
-  ocultar = false;
-  peliculasProximosEstrenos = [
-    // {
-    //   titulo: 'avengers: endgame',
-    //   fechaLanzamiento: new Date(),
-    //   precio: 1400.99,
-    // },
-    // {
-    //   titulo: 'Inception',
-    //   fechaLanzamiento: new Date('2016-11-14'),
-    //   precio: 400.99,
-    // },
-    // {
-    //   titulo: 'Rocky',
-    //   fechaLanzamiento: new Date('2016-11-14'),
-    //   precio: 400.99,
-    // },
-  ];
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.peliculasEnCines = [
-        {
-          titulo: 'Spider man',
-          fechaLanzamiento: new Date(),
-          precio: 1400.99,
-        },
-        {
-          titulo: 'Mohana',
-          fechaLanzamiento: new Date('2016-11-14'),
-          precio: 300.99,
-        },
-      ];
-    }, 1000);
-  }
-  duplicarNumero(valor: number): number {
-    return valor * 2;
-  }
-
-  manejarRated(voto: number): void{
-    alert(voto);
-  }
+export class AppComponent{
 
 
 }
