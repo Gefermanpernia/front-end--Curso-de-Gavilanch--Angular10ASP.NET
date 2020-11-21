2619816037c3ff17cc44079b509c620df9a7e8af
Geferman Pernia <geferman.pro@gmail.com>
2020-11-20 22:39:45 -0400

    enviando imagen al servidor usando formdata

--- src/app/actores/actores.service.ts
+++ src/app/actores/actores.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { formatearFecha } from '../utilidades/utilidades';
 import { actorCreacionDTO } from './actor';
 
 @Injectable({
@@ -17,6 +18,26 @@ export class ActoresService {
    * crear
    */
   public crear(actor: actorCreacionDTO) {
-    return this.http.post(this.apiURL, actor);
+    const formData = this.construirFormData(actor);
+
+    return this.http.post(this.apiURL, formData);
+  }
+
+  private construirFormData(actor: actorCreacionDTO): FormData{
+    const formData = new FormData();
+
+    formData.append('nombre', actor.nombre);
+
+    if (actor.biografia) {
+      formData.append('biografia', actor.biografia);
+    }
+    if (actor.fechaNacimiento) {
+      formData.append('fechaNacimiento', formatearFecha(actor.fechaNacimiento));
+    }
+    if (actor.foto) {
+      formData.append('foto', actor.foto);
+    }
+
+    return formData;
   }
 }
